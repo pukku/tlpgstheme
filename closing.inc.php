ae43c153aef0738e1de6f685639a6a13aada1b0d
Ricky Morse <pukku@mac.com>
2015-08-02 16:19:30 -0400

    add social media links to footer

--- closing.inc.php
+++ closing.inc.php
@@ -6,10 +6,16 @@
 	</nav>
 </div>
 
-<footer id="tlp-footer" class="site-credits">
-	<p>&copy; 2007-2015 The Longwood Players Inc.; all rights reserved.</p>
-	<p>Website by Daniel J. Rabone, Ricky Morse, Vilas Sridharan, Martin Kemp.</p>
-	<p>Graphic design: 2008-2011 Wendy J. Perrotta; 2011-2012 Todd Yard</p>
+<footer id="tlp-footer">
+	<div id="footer-contact">
+		<p><a href="mailto:info@longwoodplayers.org"><img src="<?php get_theme_url(); ?>/images/social_media/email-blue.png" alt="email"> info@longwoodplayers.org</a>, <nobr>or join us on: <a href="https://www.facebook.com/pages/The-Longwood-Players/10772506891"><img src="<?php get_theme_url(); ?>/images/social_media/facebook.png" alt="FaceBook"></a> <a href="https://twitter.com/tlplayers"><img src="<?php get_theme_url(); ?>/images/social_media/twitter.png" alt="Twitter"></a> <a href="http://thelongwoodplayers.tumblr.com/"><img src="<?php get_theme_url(); ?>/images/social_media/tumblr.png" alt="Tumblr"></a></nobr></p>
+	</div>
+	<div id="site-credits">
+		<p>&copy; 2007-2015 The Longwood Players Inc.; all rights reserved.</p>
+		<p>Website by Daniel J. Rabone, Ricky Morse, Vilas Sridharan, Martin Kemp.</p>
+		<p>Graphic design: 2008-2011 Wendy J. Perrotta; 2011-2012 Todd Yard.</p>
+		<p>Social media icons from <a href="http://www.graphicsfuel.com/2013/03/popular-social-media-icons-psd-png/">GraphicsFuel</a>.</p>
+	</div>
 </footer>
 
 <?php get_footer(); ?>
