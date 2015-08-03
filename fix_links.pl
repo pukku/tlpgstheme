868a842e2fe997e00fcf20f7b9f97c250e7ba156
Ricky Morse <pukku@mac.com>
2015-08-02 20:18:33 -0400

    add ability to have source and destination for fix_links script

--- fix_links.pl
+++ fix_links.pl
@@ -1,14 +1,27 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use Getopt::Long;
+
 
 # run this program from the base of the getsimple distribution, something like:
 # perl theme/TLP/fix_links.pl
 # it will spit out a bunch of perl commands that you should then run.
 # it does *not* actually make the changes, because I'm a wimp
 
-my $source = 'localhost:8888';
-my $dest = 'gstest.longwoodplayers.org';
+
+my $source;
+my $dest;
+GetOptions('s|source=s' => \$source, 'd|dest=s' => \$dest)
+	or die("error in command line arguments\n");
+
+
+if (!defined($source) or ($source eq '')) {
+	$source = 'localhost:8888';
+}
+if (!defined($dest) or ($dest eq '')) {
+	$dest = 'gstest.longwoodplayers.org';
+}
 
 my @files = `grep -rl "$source" *`;
 
